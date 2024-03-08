d32d2664d46a1c4ae0d3fb69fec3a2661dd79722
spruiz <spruiz97@gmail.com>
2024-03-08 13:39:15 -0500

    Update admin.py

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import House, Room
+
 # Register your models here.
+class RoomAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    
+admin.site.register(Room, RoomAdmin)
+
+class HouseAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    
+admin.site.register(House, HouseAdmin)
