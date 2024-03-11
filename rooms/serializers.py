28a4099bcbdfa154b79153d6b5a4919b9e3aec8c
spruiz <spruiz97@gmail.com>
2024-03-11 19:46:48 -0400

    Update serializers.py
    corregidos los serializers para mostrar los campos con relaciones ForeingKey y ManyToMany

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 
 from rooms.models import Room, House
-
-class HouseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = House
-        fields = '__all__'
+from services.serializers import ServiceSerializer
         
 class RoomSerializer(serializers.ModelSerializer):
+    services = ServiceSerializer(many=True, read_only=True)
     class Meta:
         model = Room
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'images', 'services']
+        
+class HouseSerializer(serializers.ModelSerializer):
+    get_house_rooms = RoomSerializer(many=True, read_only=True)
+    class Meta:
+        model = House
+        fields = ['id', 'name', 'about', 'email', 'address', 'images', 'get_house_rooms']
