f85471531f28c8ffa9ca6b8180bd0e583c799b89
spruiz <spruiz97@gmail.com>
2024-03-11 15:40:20 -0400

    Update settings.py
    activando rest framework y los archivos media

--- habanasuite/settings.py
+++ habanasuite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'core',
     'rooms',
     'services',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
