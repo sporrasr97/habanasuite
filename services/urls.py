237447024fa24e5c92c31f6385eff7d070137cb6
spruiz <spruiz97@gmail.com>
2024-03-11 19:50:51 -0400

    Update urls.py
    corregida la url para mostrar un servicio especifico

--- services/urls.py
+++ services/urls.py
@@ -3,5 +3,5 @@ from services import views
 
 urlpatterns = [
     path('services/', views.ServicesList.as_view(), name='services-list'),    
-    path('service/<int:pk>', views.ServiceDetail.as_view(), name='service-detail'),    
+    path('services/<int:pk>', views.ServiceDetail.as_view(), name='service-detail'),    
 ]
