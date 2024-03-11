855b7abd927029a82600f6cb529b6caf483d8811
spruiz <spruiz97@gmail.com>
2024-03-11 16:38:14 -0400

    Update urls.py
    incluidas urls app services
    activadas urls para archivos medias

--- habanasuite/urls.py
+++ habanasuite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,11 @@ urlpatterns = [
     
     #app rooms
     path('', include('rooms.urls')),
+    
+    #app services
+    path('', include('services.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
