fca9e3b65d34112e49f49a0687e8a5b6d6704d57
spruiz <spruiz97@gmail.com>
2024-03-11 19:47:21 -0400

    Update urls.py
    creada url para obtener detalles de una habitacion

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,5 +3,6 @@ from rooms import views
 
 urlpatterns = [
     path('rooms/', views.RoomListView.as_view(), name='room-list'),    
+    path('rooms/<int:pk>', views.RoomDetailView.as_view(), name='room-detail'),    
     path('house/<int:pk>', views.HouseDetailView.as_view(), name='house-detail'),    
 ]
