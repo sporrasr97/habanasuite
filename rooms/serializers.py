694ed9f2b30702f73c8f3b597273eb72d8b62127
spruiz <spruiz97@gmail.com>
2024-03-11 15:42:08 -0400

    Update serializers.py
    serializer para el modelo room

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -6,3 +6,8 @@ class HouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = House
         fields = '__all__'
+        
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
