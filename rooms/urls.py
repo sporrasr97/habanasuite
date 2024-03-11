cdeaa4eeb4c100b547fa161eade7387dcafca9f8
spruiz <spruiz97@gmail.com>
2024-03-11 11:55:52 -0400

    Update urls.py
    urls para el listado de rooms y los detalles de la casa

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from rooms import views
 
 urlpatterns = [
-    # path('', views., name=''),    
+    path('rooms/', views.RoomListView.as_view(), name='room-list'),    
+    path('house/<int:pk>', views.HouseDetailView.as_view(), name='house-detail'),    
 ]
