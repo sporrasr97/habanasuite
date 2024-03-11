e630d79cdcfd7172d13e037086c5d93e75225525
spruiz <spruiz97@gmail.com>
2024-03-11 11:54:44 -0400

    Update views.py
    CBV que devuelve json con los datos de la casa

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from rest_framework import generics
+
+from rooms.models import Room, House
+from rooms.serializers import HouseSerializer
 
 # Create your views here.
+class RoomListView(ListView):
+    model = Room
+
+class HouseDetailView(generics.RetrieveAPIView):
+    queryset = House.objects.all()
+    serializer_class = HouseSerializer
