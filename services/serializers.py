b306fa08e94af931b515c9f92ab0f3827ce17772
spruiz <spruiz97@gmail.com>
2024-03-11 16:38:36 -0400

    Update serializers.py
    serializer para los servicios creado

---
+++ services/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from services.models import Service
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = '__all__'
