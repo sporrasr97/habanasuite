281f014bf9a97d9e9541d7cccfbe5e594fc1fbfe
spruiz <spruiz97@gmail.com>
2024-03-11 16:39:43 -0400

    Update views.py
    CBV para json de lista de servicios
    CBV para json de detalles de un servicio

--- services/views.py
+++ services/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from services.models import Service
+from services.serializers import ServiceSerializer
 
 # Create your views here.
+class ServicesList(generics.ListAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    
+class ServiceDetail(generics.RetrieveAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
